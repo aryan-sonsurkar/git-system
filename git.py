194d1ad476957c00835ae4b7533731c22aa11a9b
Aryan Sonsurkar <aryansonsurkar87@gmail.com>
2026-05-06 22:26:31 +0530

    Repo Creation Success

--- git.py
+++ git.py
@@ -1,13 +1,20 @@
 from pathlib import Path
+import os
 
 def gitinit():
-    Path(".mygit").mkdir()
+    Path(".mygit").mkdir(exist_ok=True)
     Path(".mygit/objects").mkdir(parents=True)
-    Path(".mygit/refs").mkdir()
+    Path(".mygit/refs").mkdir(exist_ok=True)
     Path(".mygit/HEAD").write_text("ref: refs/main")
     Path(".mygit/index").write_text("")
+    print("Initialized empty repository")
 
 cmd = input("$")
+parts=cmd.split()
 
-if cmd=="mygit init":
-    gitinit()
+if len(parts) > 1 and parts[1]=="init" and parts[0]=="mygit":
+
+    if os.path.exists(".mygit"):
+        print("Repository already initialized")
+    else:
+        gitinit()
